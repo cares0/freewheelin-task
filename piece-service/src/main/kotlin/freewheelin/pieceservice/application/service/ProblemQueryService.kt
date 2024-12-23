69796bdd65379dd236ec0449ffcf733b0a4b62cd
cares0 <cares00000@gmail.com>
2024-12-24 08:30:23 +0900

    refactor: 문제 조회 쿼리 로직 수정
    - 비율 조정하여 반복 줄임

--- piece-service/src/main/kotlin/freewheelin/pieceservice/application/service/ProblemQueryService.kt
+++ piece-service/src/main/kotlin/freewheelin/pieceservice/application/service/ProblemQueryService.kt
@@ -7,9 +7,9 @@ import freewheelin.pieceservice.application.port.outbound.ProblemConditionQueryP
 import freewheelin.pieceservice.application.port.outbound.ProblemGroupQueryPort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import kotlin.math.floor
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.round
 
 @Service
 @Transactional(readOnly = true)
@@ -22,20 +22,20 @@ class ProblemQueryService(
         condition: ProblemQueryCondition,
     ): List<ProblemQueryResult> {
         // 카테고리 별 가용 가능한 문제 수를 구합니다.
-        val categoryToAvailableCountMap = getAvailableProblemCountPerCategory(condition)
+        val availableCountPerCategory = calculateAvailableProblemCountPerCategory(condition)
         
         // 총 조회해야 할 문제 수를 초기화 합니다.
-        val totalProblemCountToQuery = initTotalProblemCountToQuery(categoryToAvailableCountMap, condition.totalCount)
+        val totalProblemCountToQuery = initTotalProblemCountToQuery(availableCountPerCategory, condition.totalCount)
         
         // 카테고리 별로 문제를 할당합니다.
-        val categoryToAllocatedCountMap = allocatePerCategory(
+        val allocatedCountPerCategory = allocateCountPerCategory(
             mainCategory = condition.levelCategory,
             totalProblemCountToQuery = totalProblemCountToQuery,
-            categoryToAvailableCountMap = categoryToAvailableCountMap
+            availableCountPerCategory = availableCountPerCategory
         )
 
         // 할당된 정보를 바탕으로 문제를 쿼리합니다.
-        return queryByConditionWithAllocation(categoryToAllocatedCountMap, condition)
+        return queryProblemsByAllocation(allocatedCountPerCategory, condition)
             .sortedWith(compareBy(
                 { it.unitCode },
                 { it.problemType },
@@ -43,96 +43,116 @@ class ProblemQueryService(
             ))
     }
 
-    private fun getAvailableProblemCountPerCategory(condition: ProblemQueryCondition): Map<ProblemLevelCategory, Int> {
+    private fun calculateAvailableProblemCountPerCategory(condition: ProblemQueryCondition): Map<ProblemLevelCategory, Int> {
         // 레벨 별 가용 가능한 문제 수를 구합니다.
         val levelToAvailableCountMap = problemGroupQueryPort.queryAvailableProblemCountGroupedByLevel(
             unitCodes = condition.unitCodeList,
             problemType = condition.type,
         )
 
-        // 레벨로 그룹지어진 Map을, 카테고리로 그룹을 바꾸어 카테고리 별로 가용 가능한 문제 수를 구합니다.
-        return levelToAvailableCountMap.entries
-            .groupBy { (level, _) -> ProblemLevelCategory.fromLevel(level) }
-            .mapValues { (_, entries) -> entries.sumOf { it.value } }
+        // 카테고리에 해당되는 level을 모두 합산하여 카테고리로 그룹지어진 가용 가능한 문제 수를 구합니다.
+        return ProblemLevelCategory.entries.associateWith { category ->
+            category.levelRange.sumOf { level -> levelToAvailableCountMap[level] ?: 0 }
+        }
     }
     
     private fun initTotalProblemCountToQuery(
-        categoryToAvailableCountMap: Map<ProblemLevelCategory, Int>,
+        availableCountPerCategory: Map<ProblemLevelCategory, Int>,
         requestedTotalCount: Int,
     ): Int {
         // 총 가용 가능한 문제수를 구합니다.
-        val totalAvailableCount = categoryToAvailableCountMap.values.sum()
+        val totalAvailableCount = availableCountPerCategory.values.sum()
 
         // 총 가용 가능한 문제 수와, 요청된 총 문제 수 중 작은 값을 남은 문제 수로 초기화 합니다.
         return min(requestedTotalCount, totalAvailableCount)
     }
 
-    private fun allocatePerCategory(
+    private fun allocateCountPerCategory(
         mainCategory: ProblemLevelCategory,
         totalProblemCountToQuery: Int,
-        categoryToAvailableCountMap: Map<ProblemLevelCategory, Int>
+        availableCountPerCategory: Map<ProblemLevelCategory, Int>
     ): MutableMap<ProblemLevelCategory, Int> {
         // 남은 문제수를 초기화 합니다.
         var remainingCount = totalProblemCountToQuery
 
         // 카테고리 별로 할당된 문제 수를 담는 Map을 초기화 합니다.
-        val categoryToAllocatedCountMap = mutableMapOf<ProblemLevelCategory, Int>()
-        // 카테고리 별로 가용 가능한 남은 문제수를 저장하는 Map을 초기화 합니다.
-        val categoryToRemainingAvailableCountMap = categoryToAvailableCountMap.toMutableMap()
+        val allocatedCountPerCategory = mutableMapOf<ProblemLevelCategory, Int>()
 
-        // 비율이 높은 순으로 카테고리를 정렬하여, 우선 순위를 구합니다.
-        val categoryPriority = getCategoryPriorityFromRatio(mainCategory.getRatioMap())
+        // 카테고리 별로 가용 가능한 남은 문제수를 저장하는 Map을 초기화 합니다.
+        val remainingAvailableCountPerCategory = availableCountPerCategory.toMutableMap()
+
+        // 카테고리 별 가용 가능한 문제가 없는 경우, 비율을 조정하여 조정된 비율을 담는 Map을 만듭니다.
+        val adjustedRatioMap = mainCategory.getRatioMap().toMutableMap()
 
         // 남은 문제 수가 없어질 때 까지(문제가 모두 할당 될 때 까지) 반복합니다.
         while (remainingCount > 0) {
-            // 우선 할당해야 할 카테고리 순서대로 반복합니다.
-            categoryPriority.forEach { category ->
-                // 카테고리 별 가용 가능한 문제 수를 구합니다.
-                val availableCount = categoryToRemainingAvailableCountMap[category] ?: 0
+            // 가용 가능한 문제가 없는 카테고리를 없애고, 나머지 카테고리 안에서 비율을 조정합니다.
+            adjustRatiosForUnavailableCategories(adjustedRatioMap, remainingAvailableCountPerCategory)
 
-                // 만약 가용 가능한 문제 수가 없거나, 남아있는 문제 수가 없다면 계산 로직을 타지 않습니다.
-                if (availableCount <= 0 || remainingCount <= 0) return@forEach
+            // 이번 루프에서 비율에 적용되어야 하는 기준 문제 수를 초기화 합니다.
+            val baseProblemCountInLoop = remainingCount
 
-                // 남아있는 문제 수와, 카테고리에 할당 된 비율을 통해 할당 가능한 수를 구합니다.
-                val allocableCount = getAllocableCount(
+            // 카테고리 별 가용 가능한 문제 수를 구합니다.
+            adjustedRatioMap.forEach { (category, ratio) ->
+                allocateCountToCategory(
+                    category = category,
                     remainingCount = remainingCount,
-                    ratio = mainCategory.getRatioMap()[category]!!,
-                )
-
-                // 가용 가능한 수와, 할당 가능한 수 중 최소 값을 할당할 수로 계산합니다.
-                val allocatedCount = min(availableCount, allocableCount)
-
-                // 카테고리 별 할당할 문제 수에 할당할 수를 추가합니다.
-                categoryToAllocatedCountMap.merge(category, allocatedCount) { old, new -> old + new }
-
-                // 남은 문제 수에 할당한 수를 차감합니다.
-                remainingCount -= allocatedCount
-
-                // 가용 가능한 문제 수에서 할당한 문제 수를 차감합니다.
-                categoryToRemainingAvailableCountMap[category] = availableCount - allocatedCount
+                    baseCount = baseProblemCountInLoop,
+                    ratio = ratio,
+                    remainingAvailableCountPerCategory = remainingAvailableCountPerCategory,
+                    allocatedCountPerCategory = allocatedCountPerCategory,
+                ).also { allocated ->
+                    remainingCount -= allocated
+                }
             }
+
         }
-        return categoryToAllocatedCountMap
+        return allocatedCountPerCategory
     }
 
-
-    private fun getCategoryPriorityFromRatio(
-        ratioMap: Map<ProblemLevelCategory, Double>
-    ): List<ProblemLevelCategory> {
-        return ratioMap.entries
-            .sortedByDescending { (_, ratio) -> ratio }
-            .map { (category, _) -> category }
-    }
-
-    private fun getAllocableCount(
+    private fun allocateCountToCategory(
+        category: ProblemLevelCategory,
         remainingCount: Int,
+        baseCount: Int,
+        ratio: Double,
+        remainingAvailableCountPerCategory: MutableMap<ProblemLevelCategory, Int>,
+        allocatedCountPerCategory: MutableMap<ProblemLevelCategory, Int>
+    ): Int {
+        val availableCount = remainingAvailableCountPerCategory[category] ?: 0
+        if (availableCount <= 0 || remainingCount <= 0) return 0
+
+        val allocableCount = calculateAllocableCount(baseCount, ratio)
+        val allocatedCount = min(remainingCount, min(availableCount, allocableCount))
+
+        allocatedCountPerCategory.merge(category, allocatedCount) { old, new -> old + new }
+        remainingAvailableCountPerCategory[category] = availableCount - allocatedCount
+
+        return allocatedCount
+    }
+
+    private fun adjustRatiosForUnavailableCategories(
+        adjustedRatioMap: MutableMap<ProblemLevelCategory, Double>,
+        remainingAvailableCountPerCategory: Map<ProblemLevelCategory, Int>
+    ) {
+        val unavailableCategories = remainingAvailableCountPerCategory
+            .filterValues { it == 0 }
+            .keys
+
+        unavailableCategories.forEach(adjustedRatioMap::remove)
+        val totalRemainingRatio = adjustedRatioMap.values.sum()
+
+        adjustedRatioMap.replaceAll { _, ratio -> ratio / totalRemainingRatio }
+    }
+
+    private fun calculateAllocableCount(
+        baseCount: Int,
         ratio: Double,
     ): Int {
-        // 남아있는 문제 수와 비율에 비례한 할당 가능한 문제 수를 구합니다. 만약 1보다 작은 경우, 1개로 계산합니다.
-        return max(1, floor(remainingCount * ratio).toInt())
+        // 비율에 비례한 할당 가능한 문제 수를 구합니다. 만약 1보다 작은 경우, 1개로 계산합니다.
+        return max(1, round(baseCount * ratio).toInt())
     }
 
-    private fun queryByConditionWithAllocation(
+    private fun queryProblemsByAllocation(
         categoryToAllocatedCountMap: MutableMap<ProblemLevelCategory, Int>,
         condition: ProblemQueryCondition
     ) = categoryToAllocatedCountMap.flatMap { (category, allocatedCount) ->
